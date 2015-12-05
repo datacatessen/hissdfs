821104d6eb8fc67f66a5dac0aa80d92d0c0e9a3d
Adam J. Shook <adamjshook@gmail.com>
2015-12-05 15:31:47 -0500

    Add configuration via file

--- dcd.py
+++ dcd.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python
 
 # System imports
-import os.path as path
+import json
 import sys
+from os import path
 from socket import gethostname
 
 from rpyc.utils.server import ThreadedServer
 # Local imports
-from Utils import _mkdirp
 from NameServer import NameServer
+
+required_params = ['nameserver.host', 'nameserver.port']
+
+
+def _validate(config):
+    for key in required_params:
+        if not key in config:
+            print "missing %s in config" % key
+            sys.exit(1)
 
 
 def start_name_service(port):
@@ -21,12 +30,20 @@ def start_name_service(port):
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
-        print "usage: python dcd.py <service> <port>"
-    else:
-        service = sys.argv[1]
-        port = int(sys.argv[2])
+        print "usage: python dcd.py <config> <service>"
+        sys.exit(1)
 
-        if service == "nameserver":
-            start_name_service(port)
-        else:
-            print "error: unknown service %s" % service
+    config_file = sys.argv[1]
+    service = sys.argv[2]
+
+    if not path.exists(config_file):
+        print "config file %s not found" % config_file
+        sys.exit(1)
+
+    config = json.load(open(config_file, 'r'))
+    _validate(config)
+
+    if service == "nameserver":
+        start_name_service(config['nameserver.port'])
+    else:
+        print "error: unknown service %s" % service
